006098db2208bb5f5992b5f6355d3ca7fca74c23
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-11-15 00:38:03 +0530

    Feat: add production URL with localhost CORS error

--- server.js
+++ server.js
@@ -5,25 +5,27 @@ const app = express();
 const cors = require('cors');
 const PORT = 8000;
 
-app.use(cors());
+app.use(cors({
+    origin: ['https://moviesphere2660.vercel.app/', 'http://localhost:5173'],
+}));
 
 app.get('/api/*', async (req, res) => {
-  try {
+    try {
 
-    const url = `${process.env.BASE_URL}${req.path.replace('/api', '')}`;
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
-      },
-      params: {
-        api_key: process.env.API_KEY,
-        ...req.query
-      }
-    });
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch data' });
-  }
+        const url = `${process.env.BASE_URL}${req.path.replace('/api', '')}`;
+        const response = await axios.get(url, {
+            headers: {
+                Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+            },
+            params: {
+                api_key: process.env.API_KEY,
+                ...req.query
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
 });
 
 app.listen(PORT, () => console.log(`Proxy server running on http://localhost:${PORT}`));
